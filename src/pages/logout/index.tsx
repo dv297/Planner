cb5b2eecafa4fb338c62ed60e2b092dcda4c445b
Daniel Vu <dvv297@gmail.com>
2022-09-27 17:17:38 -0500

    Add layout to logout page

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -1,17 +1,30 @@
 import Link from 'next/link';
+import { ReactNode } from 'react';
+
+import LandingPageLayout from '../../components/LandingPageLayout';
 
 const Logout = () => {
   return (
     <div className="py-32 flex flex-col justify-center items-center">
-      <h1 className="mb-6">Thanks for using Planner!</h1>
+      <h1 className="mb-6 text-3xl font-bold text-primary">
+        Thanks for using Planner!
+      </h1>
       <p className="mb-2">You have been signed out.</p>
       <p className="mb-2">
         Use the link below to go back to the home page or feel free to close
         your browser or tab.
       </p>
-      <Link href="/">Home</Link>
+      <Link href="/">
+        <span className="text-2xl font-bold text-primary underline mt-4 cursor-pointer">
+          Home
+        </span>
+      </Link>
     </div>
   );
+};
+
+Logout.getLayout = function getLayout(page: ReactNode) {
+  return <LandingPageLayout>{page}</LandingPageLayout>;
 };
 
 export default Logout;
